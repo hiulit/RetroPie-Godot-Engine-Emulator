8eb8e92fa7d3576b6a1a59b573a30d118a7e5a82
hiulit <hiulit@gmail.com>
2019-03-27 11:19:48 +0100

    Fixed paths and naming

--- install-godot-engine.sh
+++ install-godot-engine.sh
@@ -20,6 +20,8 @@ readonly SCRIPT_VERSION="1.0.0"
 readonly SCRIPT_NAME="$(basename "$0")"
 readonly RP_DIR="$home/RetroPie"
 readonly RP_ROMS_DIR="$RP_DIR/roms"
+readonly REPO_SCRIPTMODULE_FILE="https://raw.githubusercontent.com/hiulit/RetroPie-Godot-Game-Engine-Emulator/master/scriptmodules/emulators/godot-engine.sh"
+
 RPS_DIR="$home/RetroPie-Setup"
 
 if [[ -n "$1" ]]; then
@@ -34,21 +36,22 @@ if [[ ! -d "$RPS_DIR" ]]; then
 	exit 1
 fi
 
-echo "Installing 'godot' scriptmodule ..."
-cp -R "scriptmodules/emulators/godot-engine.sh" "$RPS_DIR/scriptmodules/emulators"
+echo "Installing 'godot-engine.sh' scriptmodule ..."
+
+curl -s "$REPO_SCRIPTMODULE_FILE" > "$RPS_DIR/scriptmodules/emulators/godot-engine.sh"
 return_value="$?"
 if [[ "$return_value" -eq 0 ]]; then
-	echo "'godot' scriptmodule installed successfully!"
+	echo "'godot-engine.sh' scriptmodule installed successfully!"
 	echo
 	echo "Installation"
 	echo "------------"
-	echo "To install 'godot' run 'sudo $RPS_DIR/retropie_setup.sh'."
+	echo "To install 'godot-engine' run 'sudo $RPS_DIR/retropie_setup.sh'."
 	echo
 	echo "Go to:"
 	echo
 	echo "|- Manage packages"
 	echo "  |- Manage optional packages"
-	echo "    |- godot"
+	echo "    |- godot-engine"
 	echo "      |- Install from source"
 	echo
 	echo "After installation information"
@@ -56,5 +59,5 @@ if [[ "$return_value" -eq 0 ]]; then
 	echo "Copy your Godot games to '$RP_ROMS_DIR/godot'."
 	echo "Godot game extensions: .pck .zip."
 else
-	echo "ERROR: Couldn't install 'godot' scriptmodule." >&2
+	echo "ERROR: Couldn't install 'godot-engine.sh' scriptmodule." >&2
 fi
