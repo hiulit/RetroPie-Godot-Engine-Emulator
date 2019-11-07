947b48543752c20b8c0ad23a4957bd500e0f8fa9
hiulit <hiulit@gmail.com>
2019-11-07 15:08:02 +0100

    Fix ls functions and added new dialog for adding virtual keyboards

--- scriptmodules/emulators/godot-engine.sh
+++ scriptmodules/emulators/godot-engine.sh
@@ -27,6 +27,22 @@ godot_versions=(
     "3.1.1"
 )
 
+function _dialog_yesno() {
+    local title="$1"
+    local message="$2"
+    local dialog_height="$3"
+    local dialog_width="$4"
+    [[ -z "$title" ]] && echo "ERROR: '${FUNCNAME[0]}' needs a title as an argument!" && exit 1
+    [[ -z "$message" ]] && echo "ERROR: '${FUNCNAME[0]}' needs a message as an argument!" && exit 1
+    [[ -z "$dialog_height" ]] && dialog_height=8
+    [[ -z "$dialog_width" ]] && dialog_width="$DIALOG_WIDTH"
+    dialog \
+        --backtitle "$DIALOG_BACKTITLE" \
+        --title "$1" \
+        --yesno "$2" "$dialog_height" "$dialog_width" 2>&1 >/dev/tty
+}
+
+
 function _download_file() {
     local build_file="$file"
 
@@ -76,7 +92,9 @@ function sources_godot-engine() {
 
 
 function install_godot-engine() {
-    md_ret_files=($(ls .))
+    if [[ -d "$md_build" ]]; then
+        md_ret_files=($(ls "$md_build"))
+    fi
 }
 
 
@@ -90,7 +108,22 @@ function configure_godot-engine() {
     local index
     local version
 
-    bin_files=($(ls .))
+    local use_frt="$1"
+    local frt_keyboard="$2"
+
+    if [[ -d "$md_inst" ]]; then
+        bin_files=($(ls "$md_inst"))
+    fi
+
+    if isPlatform "x86"; then
+        id="x86"
+    elif isPlatform "aarch64"; then
+        id="frt-arm64"
+    elif isPlatform "rpi1"; then
+        id="frt-rpi0-1"
+    elif isPlatform "rpi2" || isPlatform "rpi3"; then
+        id="frt-rpi2-3"
+    fi
 
     for index in "${!bin_files[@]}"; do
         default=0
@@ -99,8 +132,54 @@ function configure_godot-engine() {
         version="${bin_files[$index]}"
         version="$(echo $version | cut -d'_' -f 2)"
         
-        addEmulator "$default" "$md_id-$version-$id" "godot-engine" "$md_inst/${bin_files[$index]} --main-pack %ROM%"
+        if [[ "$id" != "x86" && $use_frt -eq 1 ]]; then
+            addEmulator "$default" "$md_id-$version-$id" "godot-engine" "FRT_KEYBOARD_ID='$frt_keyboard' $md_inst/${bin_files[$index]} --main-pack %ROM%"
+        else
+            addEmulator "$default" "$md_id-$version-$id" "godot-engine" "$md_inst/${bin_files[$index]} --main-pack %ROM%"
+        fi
     done
 
     addSystem "godot-engine" "Godot" ".pck .zip"
+}
+
+function gui_godot-engine() {
+    local i=1
+    local options=()
+    local cmd
+    local choice
+
+    local use_frt=0
+    local frt_keyboard
+
+    dialog \
+        --backtitle "Godot - Game Engine Configuration" \
+        --title "" \
+        --yesno "Would you like you use a GPIO/Virtual keyboard?" 10 60 2>&1 >/dev/tty
+
+    if [[ "$?" -eq 0 ]]; then
+        while IFS= read -r line; do
+            line="$(echo "$line" | sed -e 's/^"//' -e 's/"$//')" # Remove leading and trailing double quotes.
+            options+=("$i" "$line")
+            ((i++))
+        done < <(cat "/proc/bus/input/devices" | grep "N: Name" | cut -d= -f2)
+
+        cmd=(dialog \
+            --backtitle "Godot - Game Engine Configuration" \
+            --title "" \
+            --ok-label "OK" \
+            --cancel-label "Exit" \
+            --menu "Choose an option." \
+            15 60 15)
+
+        choice="$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)"
+
+        if [[ "$?" -eq 0 ]]; then
+            use_frt=1
+            frt_keyboard="${options[choice*2-1]}"
+            configure_godot-engine "$use_frt" "$frt_keyboard"
+        else
+            use_frt=0
+            configure_godot-engine "$use_frt"
+        fi
+    fi
 }
